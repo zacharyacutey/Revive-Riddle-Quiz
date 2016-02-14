291c079a9cd85b718c08a6eb1927414b293272bf
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-14 13:16:27 -0500

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -28,5 +28,27 @@ Clear-Host;
 Echo-Host "DO NOT PLAY WITH EARBUDS OR HEADPHONES IN. I AM NOT RESPONSIBLE FOR ANY HEARING DAMAGE.";
 Echo-Host "";
 Echo-Host "PLAY AT YOUR OWN RISK OF BEING CAUGHT BY A TEACHER. I AM NOT RESPONSIBLE FOR ANYONE WHO GETS IN TROUBLE!";
-
-
+Pause;
+Clear-Host;
+if($id -eq "Sack")
+{
+  while($true)
+  {
+    Echo-Host "MMMMmmmmmmmmmmmmmmmmmmm.................";
+    Echo-Host "Try again";
+    Echo-Host "press enter to continue to the program!";
+    Pause | Out-Null;
+    Clear-Host;
+    Echo-Host "you will not";
+    Echo-Host "YES";
+    Echo-Host "But seriously, you can press enter to continue now!";
+    Pause | Out-Null;
+    Clear-Host;
+    Echo-Host "haha!!";
+    Start-Sleep 2;
+    Echo-Host "Dude seriously, just press enter to start the program that I made!";
+    Pause | Out-Null;
+  }
+}
+    
+  }
